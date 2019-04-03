2f4ec8a6d18d2ef02fea89849afa2f65b9396e3b
dstarrtrey <dstarrtrey@gmail.com>
2019-04-03 13:55:12 -0700

    Adds dependencies, separates db into modules, adds mock data to database, initializes graphql server

---
+++ models/user.js
@@ -0,0 +1,66 @@
+const bcrypt = require("bcrypt-nodejs");
+
+module.exports = function(sequelize, DataTypes) {
+  const User = sequelize.define("User", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    accountType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      }
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    calendar: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    permissions: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+  },
+  {
+    freezeTableName: true,
+    instanceMethods: {
+        generateHash(password) {
+            return bcrypt.hash(password, bcrypt.genSaltSync(8));
+        },
+        validPassword(password) {
+            return bcrypt.compare(password, this.password);
+        }
+    }
+  });
+
+  return User;
+};
