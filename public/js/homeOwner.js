7dd35e8ca81b5093de194cea0398255a003f7328
dstarrtrey <dstarrtrey@gmail.com>
2019-04-06 10:49:46 -0700

    Modulates state codes variable, edits id tags on register and syntax changes on js

--- public/js/homeOwner.js
+++ public/js/homeOwner.js
@@ -1,16 +1,16 @@
-let states = ['AL','AK','AS','AZ','AR','CA','CO','CT','DE','DC','FM','FL','GA','GU','HI','ID','IL','IN','IA','KS','KY','LA','ME','MH','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','MP','OH','OK','OR','PW','PA','PR','RI','SC','SD','TN','TX','UT','VT','VI','VA','WA','WV','WI','WY']
-
-let btn;
+const states = require("./statecodes");
 
 for (let i = 0; i < states.length; i++) {
-    $("#dDownMenu").append(`<button class="dropdown-item" type="button">${states[i]}</button>`);
+    $("#dDownMenu").append(
+        `<button class="dropdown-item" type="button">${states[i]}</button>`
+    );
 }
 
-let dDownBtn = "Dropdown"
+let dDownBtn = "Dropdown";
 
 $("#dDownBtn").text(dDownBtn);
 
-$("#dDownBtn").on("click", function() {
+$("#dDownBtn").on("click", function () {
     dDownBtn = $(this.text);
     console.log(dDownBtn);
     return dDownBtn;
