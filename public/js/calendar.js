adc4298d54ec2b299d9741a565a81efccf67af1f
dstarrtrey <dstarrtrey@gmail.com>
2019-04-05 18:41:23 -0700

    Modifies authentication and adds calendar pagfiles

---
+++ public/js/calendar.js
@@ -0,0 +1,19 @@
+// const cookie = document.cookie;
+
+// var query = `query RollDice($dice: Int!, $sides: Int) {
+//   rollDice(numDice: $dice, numSides: $sides)
+// }`;
+
+// fetch('/graphql', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Accept': 'application/json',
+//   },
+//   body: JSON.stringify({
+//     query,
+//     variables: { dice, sides },
+//   })
+// })
+//   .then(r => r.json())
+//   .then(data => console.log('data returned:', data));
